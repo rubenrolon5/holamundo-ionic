132a2a8305fc3eb37e66bbfbb7ac65786b8e09e2
ruben benitez <rubendariorlonbenitez@gmail.com>
2022-10-26 15:08:34 -0300

    Tarea hola mundo ionic

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+nombre:string='';
+  constructor(
+    private controladorToast: ToastController
+  ) {}
+
+  async saludar(){
+    const toast = await this.controladorToast.create({
+      message:'Hola'+this.nombre+'!',
+      duration:2000,
+      position:'bottom'
+    });
+    await toast.present();
+  }
+}
